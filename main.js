b4e8c015b6f8e54f226562b0d0f16ca7b98590f4
Francis Sanchez <francis.albert.2009@gmail.com>
2024-12-17 15:11:09 -0800

    Created scene hierarchy

---
+++ main.js
@@ -0,0 +1,43 @@
+import * as THREE from "three";
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const rootNode = new THREE.Object3D();
+scene.add(rootNode);
+
+let count = 6;
+for (let i = 0; i < 6; i++) {
+  const baseNode = new THREE.Object3D();
+  baseNode.rotation.y = i * ((2 * Math.PI) / count);
+  rootNode.add(baseNode);
+
+  const artwork = new THREE.Mesh(
+    new THREE.BoxGeometry(3, 2, 0.1),
+    new THREE.MeshBasicMaterial({ color: 0xf08080 })
+  );
+
+  artwork.position.z = -4;
+  baseNode.add(artwork);
+}
+
+function animate() {
+  rootNode.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
